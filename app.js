6a7d8d378ebe7a85a73f39c4e945cf3b724cfd05
Jake Albaugh <jake.albaugh@gmail.com>
2022-01-04 00:59:47 +0000

    ready

--- app.js
+++ app.js
@@ -11,31 +11,55 @@ const $groupB = document.getElementById("group-b");
 const $generation = document.getElementById("generation");
 const $level = document.getElementById("level");
 
-const stages = [
+const stages = generateStages([
   { guesses: 1, start: 3, end: 5 },
   { guesses: 2, start: 4, end: 7 },
   { guesses: 3, start: 5, end: 8 },
-];
+]);
+
 const maxItems = 10;
 const maxGuesses = 3;
 const rangeItems = maxItems * 2;
 const cores = rangeItems + maxGuesses;
-const engine = new SeededRandomEngine({
-  cores,
-  history: 1,
-  seed: "here",
-});
+let engine = generateEngine();
+
+function generateEngine() {
+  return new SeededRandomEngine({
+    cores,
+    history: 1,
+    seed: "here",
+  });
+}
 
 const config = {
-  level: 0,
+  level: 1,
 };
 
 $groupA.addEventListener("change", groupChange);
 $groupB.addEventListener("change", groupChange);
+$level.addEventListener("change", () => {
+  const level = parseInt($level.value);
+  if (!$level.value) {
+    return;
+  }
+  generate(level);
+});
+$generation.addEventListener("change", () => {
+  const generation = parseInt($generation.value);
+  if (!$generation.value) {
+    return;
+  }
+  if (generation < engine.generation) {
+    engine = generateEngine();
+  } else {
+    engine.ff(generation - 1);
+  }
+  generate();
+});
 
 $prev.addEventListener("click", () => generate(config.level - 1));
 $next.addEventListener("click", () => generate(config.level + 1));
-generate();
+generate(1);
 
 function groupChange() {
   if ($groupA.checked) {
@@ -48,13 +72,15 @@ function groupChange() {
   }
 }
 
-function generate(nextLevel = 0) {
+function generate(nextLevel) {
   engine.generate();
-
-  config.level = Math.max(nextLevel, 0);
-  $level.value = config.level;
   $generation.value = engine.generation;
-  if (engine.generation % 2 === 0) {
+
+  if (nextLevel !== undefined) {
+    config.level = Math.max(nextLevel, 1);
+    $level.value = config.level;
+  }
+  if (config.level % 2 === 0) {
     document.body.classList.remove("odd");
     document.body.classList.add("even");
   } else {
@@ -65,27 +91,39 @@ function generate(nextLevel = 0) {
   const constraints = extractConstraints();
   const terms = extractTerms();
 
-  const termCount = 3 + (config.level % 6);
+  const { guesses, quantity } =
+    stages[Math.min(stages.length, config.level) - 1];
+
+  const constraintsDisplay = [...constraints].slice(0, guesses);
+  const termsDisplay = shuffle([...terms].slice(0, quantity));
+  const limitedTermsDisplay = [...terms].slice(0, guesses);
 
   $main.innerHTML = `
-    <div class="view-1">VIEW 1 ${constraints.join(", ")}<br>${terms.join(
-    ", "
-  )}</div>
-    <div class="view-2">VIEW 2 ${constraints.join(", ")}<br>${terms.join(
-    ", "
-  )}</div>`;
+    <div class="view-1">
+      <ul>
+        ${termsDisplay
+          .map((term) => `<li><strong>${term}</strong></li>`)
+          .join("")}
+      </ul>
+    </div>
+    <div class="view-2">
+      <ul>
+        ${limitedTermsDisplay
+          .map(
+            (term, i) =>
+              `<li><strong>${term}</strong><br><em>${constraintsDisplay[i]}</em></li>`
+          )
+          .join("")}
+      </ul>
+    </div>`;
 }
 
 function extractConstraints() {
   const values = engine.values();
-  const possibleStraints = constraints.simple;
+  const possible = constraints.simple;
   const straints = [];
   for (let i = 0; i < maxGuesses; i++) {
-    straints.push(
-      possibleStraints[
-        Math.floor(values[maxItems + i] * possibleStraints.length)
-      ]
-    );
+    straints.push(possible[Math.floor(values[maxItems + i] * possible.length)]);
   }
   return straints;
 }
@@ -99,4 +137,33 @@ function extractTerms() {
     terms.push(group.splice(index, 1)[0]);
   }
   return terms;
+}
+
+function generateStages(params) {
+  const data = [];
+  params.forEach(({ guesses, start, end }) => {
+    for (let a = start; a <= end; a++) {
+      for (let i = 0; i < 3; i++) {
+        data.push({ guesses, quantity: a });
+      }
+    }
+  });
+  return data;
+}
+
+function shuffle(array) {
+  let currentIndex = array.length;
+  let randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
 }
